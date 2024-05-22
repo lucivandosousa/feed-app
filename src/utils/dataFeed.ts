4e6ac03d98a8c752010c38b32918259c71290605
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-22 08:33:27 -0300

    feat: update end create data

--- src/utils/dataFeed.ts
+++ src/utils/dataFeed.ts
@@ -1,51 +1,51 @@
-export const data = [
+export const dataFeed = [
   {
     id: 1,
-    fullName: "Maria",
+    user: "marialorem",
     timeStamp: "12:47 PM",
-    recentText: "Good Day!",
-    avatarUrl: "https://images.pexels.com/photos/1674752/pexels-photo-1674752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus nibh arcu, non finibus est auctor id. Pellentesque quis nibh facilisis, molestie nunc ac, faucibus dolor. Aliquam rhoncus dolor a urna congue, sit amet faucibus nulla laoreet. Integer eget lectus ex. Maecenas tincidunt, nibh ut euismod consequat, lectus turpis elementum felis, a ultricies erat odio eu odio. In suscipit ligula commodo aliquet tristique.",
+    feedUrl: "https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     id: 2,
-    fullName: "João",
+    user: "joaolorem",
     timeStamp: "11:40 PM",
-    recentText: "Good Day!",
-    avatarUrl: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus nibh arcu, non finibus est auctor id. Pellentesque quis nibh facilisis, molestie nunc ac, faucibus dolor. Aliquam rhoncus dolor a urna congue, sit amet faucibus nulla laoreet. Integer eget lectus ex. Maecenas tincidunt, nibh ut euismod consequat, lectus turpis elementum felis, a ultricies erat odio eu odio. In suscipit ligula commodo aliquet tristique.",
+    feedUrl: "https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     id: 3,
-    fullName: "Kleber",
+    user: "kleberlorem",
     timeStamp: "10:47 PM",
-    recentText: "Good Day!",
-    avatarUrl: "https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus nibh arcu, non finibus est auctor id. Pellentesque quis nibh facilisis, molestie nunc ac, faucibus dolor. Aliquam rhoncus dolor a urna congue, sit amet faucibus nulla laoreet. Integer eget lectus ex. Maecenas tincidunt, nibh ut euismod consequat, lectus turpis elementum felis, a ultricies erat odio eu odio. In suscipit ligula commodo aliquet tristique.",
+    feedUrl: "https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg"
   },
   {
     id: 4,
-    fullName: "Carolina",
+    user: "carolinalorem",
     timeStamp: "09:35 PM",
-    recentText: "Good Day!",
-    avatarUrl: "https://images.pexels.com/photos/1687675/pexels-photo-1687675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus nibh arcu, non finibus est auctor id. Pellentesque quis nibh facilisis, molestie nunc ac, faucibus dolor. Aliquam rhoncus dolor a urna congue, sit amet faucibus nulla laoreet. Integer eget lectus ex. Maecenas tincidunt, nibh ut euismod consequat, lectus turpis elementum felis, a ultricies erat odio eu odio. In suscipit ligula commodo aliquet tristique.",
+    feedUrl: "https://images.pexels.com/photos/462162/pexels-photo-462162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     id: 5,
-    fullName: "Marcos",
+    user: "marcoslorem",
     timeStamp: "09:35 PM",
-    recentText: "Good Day!",
-    avatarUrl: "https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus nibh arcu, non finibus est auctor id. Pellentesque quis nibh facilisis, molestie nunc ac, faucibus dolor. Aliquam rhoncus dolor a urna congue, sit amet faucibus nulla laoreet. Integer eget lectus ex. Maecenas tincidunt, nibh ut euismod consequat, lectus turpis elementum felis, a ultricies erat odio eu odio. In suscipit ligula commodo aliquet tristique.",
+    feedUrl: "https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     id: 6,
-    fullName: "Isabel",
+    user: "isabellorem",
     timeStamp: "09:35 PM",
-    recentText: "Good Day!",
-    avatarUrl: "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus nibh arcu, non finibus est auctor id. Pellentesque quis nibh facilisis, molestie nunc ac, faucibus dolor. Aliquam rhoncus dolor a urna congue, sit amet faucibus nulla laoreet. Integer eget lectus ex. Maecenas tincidunt, nibh ut euismod consequat, lectus turpis elementum felis, a ultricies erat odio eu odio. In suscipit ligula commodo aliquet tristique.",
+    feedUrl: "https://images.pexels.com/photos/620337/pexels-photo-620337.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     id: 7,
-    fullName: "Ivete",
+    user: "ivetelorem",
     timeStamp: "09:35 PM",
-    recentText: "Good Day!",
-    avatarUrl: "https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin faucibus nibh arcu, non finibus est auctor id. Pellentesque quis nibh facilisis, molestie nunc ac, faucibus dolor. Aliquam rhoncus dolor a urna congue, sit amet faucibus nulla laoreet. Integer eget lectus ex. Maecenas tincidunt, nibh ut euismod consequat, lectus turpis elementum felis, a ultricies erat odio eu odio. In suscipit ligula commodo aliquet tristique.",
+    feedUrl: "https://images.pexels.com/photos/2166711/pexels-photo-2166711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   }
 ]
---
+++ src/utils/dataStorys.ts
@@ -0,0 +1,51 @@
+export const dataStorys = [
+  {
+    id: 1,
+    fullName: "Maria",
+    timeStamp: "12:47 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://images.pexels.com/photos/1674752/pexels-photo-1674752.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+    id: 2,
+    fullName: "João",
+    timeStamp: "11:40 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+    id: 3,
+    fullName: "Kleber",
+    timeStamp: "10:47 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+    id: 4,
+    fullName: "Carolina",
+    timeStamp: "09:35 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://images.pexels.com/photos/1687675/pexels-photo-1687675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+    id: 5,
+    fullName: "Marcos",
+    timeStamp: "09:35 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+    id: 6,
+    fullName: "Isabel",
+    timeStamp: "09:35 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+    id: 7,
+    fullName: "Ivete",
+    timeStamp: "09:35 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  }
+]
