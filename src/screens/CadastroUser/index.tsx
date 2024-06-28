0362b7850ef51bd023d789109633d839c7f39b49
Lucivando Sousa <lucivandosousa@gmail.com>
2024-06-28 19:56:51 -0300

    feat: create interface of page cadastrouser

--- src/screens/CadastroUser/index.tsx
+++ src/screens/CadastroUser/index.tsx
@@ -1,8 +1,73 @@
-import { Text } from 'native-base'
-import React from 'react'
+import { Button, CheckIcon, Heading, Input, Select, VStack } from 'native-base'
+import { useState } from 'react'
 
-export default function CadastroUser() {
+export default function CadastroUser({ navigation }) {
+
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    telefone: '',
+    genero: '',
+    senha: ''
+  })
+
+  function handleInputChange(name, value) {
+    setFormData({ ...formData, [name]: value });
+  }
+
+  async function handleSubmit() {
+    console.log(formData)
+    navigation.navigate('Login');
+  }
+
   return (
-    <Text>Tela Cadastro</Text>
+    <VStack flex={1} alignItems={"center"} justifyContent={"center"} paddingX={10} space={2}>
+      <Heading size="xl" fontWeight="600" color="blue.900" textAlign={"center"} marginBottom={10}>
+        Cadastro
+      </Heading>
+      <Input
+        w={'full'}
+        placeholder='Nome'
+        onChangeText={(value) => handleInputChange('nome', value)}
+      />
+      <Input
+        w={'full'}
+        placeholder='Email'
+        onChangeText={(value) => handleInputChange('email', value)}
+      />
+      <Input
+        w={'full'}
+        placeholder='Telefone'
+        onChangeText={(value) => handleInputChange('telefone', value)}
+      />
+      <Select
+        selectedValue={formData.genero}
+        w={'full'}
+        accessibilityLabel="Selecione o gênero"
+        placeholder="Selecione o gênero"
+        _selectedItem={{
+          bg: "indigo.200",
+          endIcon: <CheckIcon size="5" />
+        }}
+        onValueChange={(value) => handleInputChange('genero', value)}
+      >
+        <Select.Item label="Masculino" value="masculino" />
+        <Select.Item label="Feminino" value="feminino" />
+        <Select.Item label="Outro" value="outro" />
+      </Select>
+      <Input
+        w={'full'}
+        type='password'
+        placeholder='Senha'
+        onChangeText={(value) => handleInputChange('senha', value)}
+      />
+      <Button
+        colorScheme={"indigo"}
+        w={'full'}
+        onPress={handleSubmit}
+      >
+        Concluir
+      </Button>
+    </VStack>
   )
 }
